1e23d71510b18b4b6fde1cc1b589f45bef4389cf
Colin Stannard <colin.stannard11@imperial.ac.uk>
2013-03-06 22:17:09 +0000

    Bloodlust mostly implemented - currently fails due to insufficient memory.

--- my_wol.pl
+++ my_wol.pl
@@ -1,42 +1,88 @@
-are_identical(X,Y):-
-X==Y.
 
-test_strategy(NumberOfGames,FirstPlayerStrategy, SecondPlayerStrategy) :-
-  run_test(0, NumberOfGames, FirstPlayerStrategy, SecondPlayerStrategy, [], [], 0).
+% Find all possible moves that can be made.
+possible_moves(Alive, OtherPlayerAlive, PossibleMoves) :-
+  findall([A,B,MA,MB],(member([A,B], Alive),
+                      neighbour_position(A,B,[MA,MB]),
+	              \+member([MA,MB],Alive),
+	              \+member([MA,MB],OtherPlayerAlive)),
+	 PossibleMoves).
 
 
-run_test(NumberOfGames, NumberOfGames, _, _,
-         Winners, Moves, RunTime) :-
-  include(are_identical('b'), Winners, PlayerOneWinners),
-  length(PlayerOneWinners,PlayerOneWins),
-  include(are_identical('r'), Winners, PlayerTwoWinners),
-  length(PlayerTwoWinners,PlayerTwoWins),
-  exclude(are_identical('b'), Winners, NoBlues),
-  exclude(are_identical('r'), NoBlues, Draws),
-  length(Draws, NumberOfDraws),
-  min_member(Shortest,Moves),
-  sumlist(Moves, TotalMoves),
-  AverageGameLength is TotalMoves / NumberOfGames,
-  delete(Moves, 250, NonExhaustiveMoves),
-  max_member(Longest, NonExhaustiveMoves),
-  AverageRunTime is RunTime / NumberOfGames,
-  write('Number of Draws: '), write(NumberOfDraws), nl, 
-  write('Number of wins for player 1 (blue): '), write(PlayerOneWins), nl, 
-  write('Number of wins for player 2 (red): '), write(PlayerTwoWins), nl,
-  write('Longest (non-exhaustive) game: '), write(Longest), nl,
-  write('Shortest game: '), write(Shortest), nl,
-  write('Average game length: '), write(AverageGameLength), nl,
-  write('Average game time: '), write(AverageRunTime), nl.
+play_possible_moves(InitialBoard, PossibleMoves, Boards) :-
+  % For all valid moves, apply the move to the current board and count how many
+  % opponent pieces are alive after the move has been made.
+  findall(
+    % Produce an object containing the move made and the resulting board
+    [CurrentMove, AfterCrankBoard],
 
-   
-run_test(GamesPlayed, NumberOfGames, FirstPlayerStrategy, SecondPlayerStrategy,
- Winners, Moves, RunTime) :-
-  statistics(runtime, [StartTime|_]),
-  play(quiet, FirstPlayerStrategy, SecondPlayerStrategy, TotalMoves, Winner),
-  statistics(runtime, [EndTime|_]),
-  Time is EndTime - StartTime,
-  NewRunTime is RunTime + Time,
-  NewGames is GamesPlayed + 1,
-  run_test(NewGames, NumberOfGames, FirstPlayerStrategy, SecondPlayerStrategy, 
-          [Winner|Winners], [TotalMoves|Moves], NewRunTime).
+    % Filtering rule
+    ( member(CurrentMove, PossibleMoves),
+      do_move(CurrentMove, InitialBoard, PreCrankBoard),
+      next_generation(PreCrankBoard, AfterCrankBoard)
+    ),
+
+    % List of generated objects
+    Boards
+  ).
+
+do_move([A,B,MA,MB], [AliveBlues, AliveReds], [NewAliveBlues, NewAliveReds]) :-
+% Blue player makes a move
+( member([A,B], AliveBlues),
+  alter_board([A,B,MA,MB], AliveBlues, NewAliveBlues),
+  NewAliveReds = AliveReds,
+  !
+);
+% Red player makes a move
+( member([A,B], AliveReds),
+  alter_board([A,B,MA,MB], AliveReds, NewAliveReds),
+  NewAliveBlues = AliveBlues,
+  !
+).
+
+
+
+count_player_pieces('b', [ActiveBlues, _], Count) :-
+  length(ActiveBlues, Count).
+
+count_player_pieces('r', [_, ActiveReds], Count) :-
+  length(ActiveReds, Count).
+
+
+opponent('b', 'r').
+opponent('r', 'b').
+
+redmin([_, Board], [_, Board1]) :-
+  count_player_pieces('r', Board, Count),
+  count_player_pieces('r', Board1, Count1),
+  Count < Count1.
+
+bluemin([_, Board], [_, Board1]) :-
+  count_player_pieces('b', Board, Count),
+  count_player_pieces('b', Board1, Count1),
+  Count < Count1.
+
+
+% This strategy chooses the next move for a player to be the one which (after 
+% Conway’s crank) produces the board state with the fewest number of opponent’s
+% pieces on the board (ignoring the player’s own pieces).
+bloodlust(Player, [AliveBlues, AliveReds], NewBoard, Move) :-
+
+  % Generate possible moves
+  ( Player == 'b',
+    possible_moves(AliveBlues, AliveReds, PossibleMoves)
+  );
+  ( Player =='r',
+    possible_moves(AliveReds, AliveBlues, PossibleMoves)
+  ),
+
+  % Simulate each possible move
+  play_possible_moves([AliveBlues, AliveReds], PossibleMoves, Boards),
+
+  ( Player == 'b',
+    min_member(bluemin, [Move, NewBoard], Boards)
+  );
+  ( Player =='r',
+    min_member(redmin, [Move, NewBoard], Boards)
+  ).
+
 
